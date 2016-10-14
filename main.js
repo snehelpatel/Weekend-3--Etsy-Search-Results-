e8fb69b473f269774bb21ebb6f007e86730c173d
Snehel Patel <snehelpatel92@gmail.com>
2016-10-14 13:24:32 -0400

    dumb boxes are more or less in place

---
+++ main.js
@@ -0,0 +1,31 @@
+// var listingBlock = [" "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "];
+//
+// function renderListing (space) {
+//   if (space === " ") {
+//     return `<div class= "box"></div>`;
+//   }
+//
+//
+// function render (listingsBlocked) {
+//   var intoHtml = []
+// }
+
+$(function() {
+  for (var i=0; i <16; i++) {
+  $( "<div class='dumb-box'>first dumb image</div>").appendTo( "#right-container" );
+
+  }
+});
+
+
+
+
+
+// $(document).ready(function() {
+//     for(var x = 0; x < 16; x++) {
+//         for(var y = 0; y < 16; y++) {
+//             var unit = $("<div class='unit'></div>");
+//             unit.appendTo('#container');
+//         }
+//     }
+// });
