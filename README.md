c7c48b7ecb0c7db67e3048fd4c27a16d1c22b82e
Snehel Patel <snehelpatel92@gmail.com>
2016-10-21 08:35:05 -0400

    turning things in

---
+++ README.md
@@ -0,0 +1,9 @@
+### Etsy Search Result page [Etsy Tea Search](https://www.etsy.com/search?q=tea)
+
+Our third weekend assignment was to recreate the first page of an Etsy search result.
+This assignment is meant to reflect our understanding of
+
+**DOM Manipulation
+**JSON navigation
+**Template literal use
+**SASS and JQUERY use for better styling
--- main.js
+++ main.js
@@ -1,17 +1,68 @@
 var results = items.results;
 
+
+var images= results.map(function(result){
+return result.Images[0].url_fullxfull;
+});
+var title = results.map(function (result){
+return result.title;
+});
+
+var prices = results.map(function (result){
+return result.price;
+});
+
+var shop = results.map(function (result){
+return result.Shop.shop_name;
+});
+
+var link = results.map (function (result) {
+  return result.url;
+});
+
+var number = items.count;
+var numberCount = number.toLocaleString('en')
+$(".count").append(`(${numberCount})`);
+
+var heart= `<img class="heart" src="./assets/heart.png">`
+var burger= `<img class="hamburger" src="./assets/hamburger.png">`
+
+// $( ".burgerHearts" ).hover(
+//   function() {
+//     $( this ).addClass( "hover" );
+//   }, function() {
+//     $( this ).removeClass("burgerHearts");
+//   }
+// );
+
+
 $(function() {
   for (var i=0; i <16; i++) {
-  $( "<div class='dumb-box'>first dumb image</div>").appendTo( "#right-container" );
+  $( `#right-container`).append(`<div class='items-box'>
+  <div class="burgerHearts"><div>${heart}${burger}</div></div>
+  <img src= "${images[i]}"/>
+  <div class= "shop">${shop[i]}</div>
+  <div class = "prices"> $${prices[i]}</div>
+   <div class= "title"><a href="${link[i]}">${title[i]}</a></div>
+
+
+   </div>`
+ );
 
   }
 });
 
-var prices = results.map(function(result){
-return result.price;
-})
+//
+// $(.title).click(gettingLinks)
+//
+// var titleLink= $(`.title`).click()
 
-var images = results.map(function(result){
-return result.Images[0].url_fullxfull
-;
-})
+
+//
+//
+//
+//
+// $(".images").hover(function(x){
+//          x.preventDefault();
+//           ;
+// });
